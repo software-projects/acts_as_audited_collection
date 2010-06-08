33a76b3c1811f8ab6e68646a86b0c5030c9cacab
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2010-06-08 15:24:22 +1000

    Added db schema and ignore the sqlite db file

--- spec/db/schema.rb
+++ spec/db/schema.rb
@@ -1,6 +1,12 @@
 ActiveRecord::Schema.define(:version => 0) do
-  # create_table :chickens, :force => true do |t|
-  #   t.column :name, :string
-  #   t.column :age, :integer
-  # end
+  create_table :parents, :force => true do |t|
+    t.string :name
+    t.timestamps
+  end
+
+  create_table :children, :force => true do |t|
+    t.string :name
+    t.references :parent
+    t.timestamps
+  end
 end
