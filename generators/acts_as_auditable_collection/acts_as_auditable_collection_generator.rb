e2144d794565c130d0dff7a34688833fda25ef3b
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2010-06-08 15:17:52 +1000

    Initial commit of empty Rails plugin.

---
+++ generators/acts_as_auditable_collection/acts_as_auditable_collection_generator.rb
@@ -0,0 +1,8 @@
+class ActsAsAuditableCollectionGenerator < Rails::Generator::NamedBase
+  def manifest
+    record do |m|
+      # m.directory "lib"
+      # m.template 'README', "README"
+    end
+  end
+end
---
+++ tasks/acts_as_auditable_collection_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :acts_as_auditable_collection do
+#   # Task goes here
+# end
