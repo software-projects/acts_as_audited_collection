58154224cda079ce00ed3e7a7ac26583d65823be
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2010-06-09 10:25:40 +1000

    Added collection_audits table to the test schema

--- spec/db/schema.rb
+++ spec/db/schema.rb
@@ -9,4 +9,13 @@ ActiveRecord::Schema.define(:version => 0) do
     t.references :test_parent
     t.timestamps
   end
+
+  create_table :collection_audits, :force => true do |t|
+    t.references :parent_record, :polymorphic => {}
+    t.references :child_record, :polymorphic => {}
+    t.references :user, :polymorphic => {}
+    t.string :username
+    t.string :action
+    t.datetime :created_at
+  end
 end
