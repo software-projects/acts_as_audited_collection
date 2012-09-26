27c62766d3472f14b2d3d632a899d1dc2c454460
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2012-09-26 18:14:02 +1000

    Added the ActiveRecord migration generator to pick up the migration number generator

--- lib/generators/audited_collection_upgrade/audited_collection_upgrade_generator.rb
+++ lib/generators/audited_collection_upgrade/audited_collection_upgrade_generator.rb
@@ -1,7 +1,12 @@
 # Released under the MIT license. See the LICENSE file for details
 
+require 'rails/generators'
+require 'rails/generators/migration'
+require 'rails/generators/active_record/migration'
+
 class AuditedCollectionUpgradeGenerator < Rails::Generators::NamedBase
   include Rails::Generators::Migration
+  extend ActiveRecord::Generators::Migration
   attr_accessor :from_version
 
   argument :from, :type => :string, :default => '', :banner => 'VERSION'
