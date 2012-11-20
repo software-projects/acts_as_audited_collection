0a15e92d65624d350672c34614a7c477f9491c57
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2012-11-21 09:56:33 +1000

    Fixed migration generator

--- lib/generators/audited_collection_migration/audited_collection_migration_generator.rb
+++ lib/generators/audited_collection_migration/audited_collection_migration_generator.rb
@@ -1,6 +1,13 @@
 # Released under the MIT license. See the LICENSE file for details
 
+require 'rails/generators'
+require 'rails/generators/migration'
+require 'rails/generators/active_record/migration'
+
 class AuditedCollectionMigrationGenerator < Rails::Generators::NamedBase
+  include Rails::Generators::Migration
+  extend ActiveRecord::Generators::Migration
+
   source_root File.join(File.dirname(__FILE__), 'templates')
 
   def manifest
